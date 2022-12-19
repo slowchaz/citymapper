a69d035f72c7366d69060c1567ba9c84255c2e56
chaz <corfitzgerald@gmail.com>
2022-12-18 19:30:10 -0600

    basic MapLibre setup

--- components/CityMap.tsx
+++ components/CityMap.tsx
@@ -1,15 +1,35 @@
-import * as React from 'react';
+import React, { useEffect, useRef } from "react";
+import maplibregl from "maplibre-gl";
+import "maplibre-gl/dist/maplibre-gl.css";
 
-interface Props {
-  message: string;
-}
 
-const CityMap: React.FC<Props> = (props) => {
+export const CityMap = () => {
+  const mapContainerRef = useRef<HTMLDivElement | null>(null);
+
+  useEffect(() => {
+    if (!mapContainerRef.current) return;
+
+    const map = new maplibregl.Map({
+      container: mapContainerRef.current,
+      style: "https://demotiles.maplibre.org/style.json",
+      center: [135.0066832, 37.9619195],
+      zoom: 4,
+    });
+
+    return () => {
+      map.remove();
+    };
+  }, [mapContainerRef]);
+
   return (
-    <div>
-      {props.message}
-    </div>
+    <div
+      ref={mapContainerRef}
+      style={{
+        width: "100%",
+        height: "100%",
+      }}
+    />
   );
 };
 
-export default CityMap;
+
